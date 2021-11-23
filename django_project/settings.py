1cdfd4408ea17ae80cb411191207a72160d1449e
mugdha273 <mugdhasharma0327@gmail.com>
2021-11-23 16:09:50 +0530

    deployment fix

--- django_project/settings.py
+++ django_project/settings.py
@@ -137,3 +137,10 @@ LOGIN_URL = 'login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)

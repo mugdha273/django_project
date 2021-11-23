3064b5a988f7c0e9a88d0b96c83085e4d0128dbe
mugdha273 <mugdhasharma0327@gmail.com>
2021-11-23 15:28:35 +0530

    last commit

--- blog/urls.py
+++ blog/urls.py
@@ -21,11 +21,13 @@ from .views import (
     PostUpdateView, 
     PostCreateView,
     PostDeleteView,
+    UserPostListView
         )
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
